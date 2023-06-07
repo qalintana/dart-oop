d51cdcabe65c12fa053c88ca77f0774f4b35eb5e
Queiroz Santana <alintana18@gmail.com>
2023-06-07 22:50:30 +0100

    Asyn dart, future delay

--- lib/promises/novo.dart
+++ lib/promises/novo.dart
@@ -2,14 +2,21 @@ void main() {
   print('Iniciou o processo');
 
   var i = 0;
+  m2();
+  p2();
   while (i < 50000000) {
     i++;
   }
 
-  m2();
   print('finalizou');
 }
 
 void m2() {
   print('funcao 2');
+}
+
+void p2() {
+  Future.delayed(Duration(seconds: 2), () {
+    print('processo p2 sendo executado');
+  });
 }
